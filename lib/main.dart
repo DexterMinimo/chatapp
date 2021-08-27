72576157d93bfe03891c17bb6e9aec3667d0cf16
DexterMinimo <lakshyakhichar@gmail.com>
2021-08-27 20:48:05 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chatapp/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Chat App",
+      home: new HomePage(),
+    );
+  }
+}
